aab9f8489b3ecfb94ed7b2313c7c47a430bd25bd
CodySingh <152055035+CodySingh@users.noreply.github.com>
2026-06-28 21:44:17 +0800

    built the car class and added borders along with movement

--- graphicProgramming/sketch.js
+++ graphicProgramming/sketch.js
@@ -1,57 +1,73 @@
 
+
+//import Matter.js Library
 let Engine = Matter.Engine;
 let World = Matter.World;
 let Bodies = Matter.Bodies;
+let Body = Matter.Body;
 
-let engine = Engine.create();
-let world = engine.world;
+//Global variables 
+let engine;
+let world;
 
+let player;
+let wallThickness = 5;
 
-class car{
-    constructor(x, y, width, height, speed, color){
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.speed = speed;
-        this.color = color;
+//arrays for objects
+let cars = [];
+let walls = [];
 
-        this.body = Bodies.rectangle(this.x, this.y, this.width, this.height);
-
-        World.add(world, this.body);
-    }
-}
-
-
-let car1 = new car(40, 350, 50, 30, 5, 'red');
-let car2 = new car(40, 400, 50, 30, 5, 'green');
-let car3 = new car(40, 450, 50, 30, 5, 'yellow');
-let car4 = new car(40, 500, 50, 30, 5, 'orange');
-
-
-
+//INITIALISATION FUNCTIONS 
 function setup(){
+
     createCanvas(1400,700);
     stroke(0);
-    strokeWeight(3);    
+    strokeWeight(3);
+
+    engine = Engine.create();
+    world = engine.world;
+
+    world.gravity.y = 0;
+    world.gravity.x = 0;
+
+    createWalls();
+    
 
 
+    //draw the player car 
+    player = new car(40, 350, 50, 30, 'red');
+    cars.push(player);
 }
 
 
 function draw(){
+
     background(220);
+
+    handleInput();
+
+    Engine.update(engine);
+
     playingArea();
     startingArea();
-    drawCar(car);
-    keyPressed();
+
+    for (let car of cars) { 
+        car.draw();
+    }
+
+    for (let wall of walls) {
+        wall.draw();
+    }
 
 }
 
+
+
+//HELER FUNCTIONS 
 function playingArea(){
 
     fill('white');
-    rect(0,0,1400,700);
+    rect(2, 2, width - 4, height - 4);
 
 }
 
@@ -63,33 +79,91 @@ function startingArea(){
     
 }
 
-function drawCar(car) {
-    fill(car1.color);
-    rect(car1.x, car1.y, car1.width, car1.height);
 
-    fill(car2.color);
-    rect(car2.x, car2.y, car2.width, car2.height);
 
-    fill(car3.color);
-    rect(car3.x, car3.y, car3.width, car3.height);
+function handleInput() {
 
-    fill(car4.color);
-    rect(car4.x, car4.y, car4.width, car4.height);
+    let speed = 5;
 
+    if (keyIsDown(UP_ARROW)) {
+        player.move(0, -speed);
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+        player.move(0, speed);
+    }
+    if (keyIsDown(LEFT_ARROW)) {
+        player.move(-speed, 0);
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+        player.move(speed, 0);
+    }
+}
+
+function createWalls() {
+
+    walls.push(new wall(width/2, wallThickness/2, width, wallThickness, 'black')); //top wall
+    walls.push(new wall(width/2, height - wallThickness/2, width, wallThickness, 'black')); //bottom wall
+    walls.push(new wall(wallThickness/2, height/2, wallThickness, height, 'black'));//left wall
+    walls.push(new wall(width - wallThickness/2, height/2, wallThickness, height, 'black'));//right wall
+}
+
+// CLASSES 
+class car{
+    constructor(x, y, w, h, color){
+
+        this.body = Bodies.rectangle(x, y, w, h, color);
+
+        this.width = w;
+        this.height = h;
+        this.color = color;
+
+        this.body = Bodies.rectangle(x, y, w, h, {restitution: 0.5, friction:0.5});
+
+        World.add(world, this.body);
+
+    }
+    
+    draw() {
+
+        push();
+        fill('red');
+        rectMode(CENTER);
+        rect(this.body.position.x, this.body.position.y, this.width, this.height);
+        rotate(this.body.angle);
+        pop();
+
+    }
+
+    move(dx, dy) {
+
+        Body.setPosition(this.body, {
+            x: this.body.position.x + dx,
+            y: this.body.position.y + dy
+        });
+
+    }
     
 }
 
-function keyPressed() {
-
-    if (keyCode === UP_ARROW) {
-        car1.y -= car1.speed;
-    } else if (keyCode === DOWN_ARROW) {
-        car1.y += car1.speed;
-    } else if (keyCode === LEFT_ARROW) {
-        car1.x -= car1.speed;
-    } else if (keyCode === RIGHT_ARROW) {
-        car1.x += car1.speed;
+class wall {
+    constructor(x, y, w, h, color) {
+  
+        this.width = w;
+        this.height = h;
+        this.color = color;
+        this.body = Bodies.rectangle(x, y, w, h, {isStatic: true, restitution:1});
+        
+        World.add(world, this.body);
+        
     }
 
-}
+    draw() {
 
+        push();
+        rectMode(CENTER);
+        fill(this.color);
+        rect(this.body.position.x, this.body.position.y, this.width, this.height);
+        pop();
+
+    }
+}
